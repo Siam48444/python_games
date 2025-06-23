0de99273ee8cd9c8b094daf6f0f64e57f7a57961
Siam <siam48444@gmail.com>
2025-06-23 20:00:35 +0600

    siam

---
+++ Space Dodge/main.py
@@ -0,0 +1,38 @@
+import pygame
+import time
+import random
+
+
+WIDTH, HEIGHT = 1000, 600
+PLAYER_WIDTH, PLAYER_HEIGHT = 40, 60
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+TITLE = pygame.display.set_caption("Space Dodge")
+
+
+def main():
+    player = pygame.Rect((WIDTH - PLAYER_WIDTH) / 2, HEIGHT - PLAYER_HEIGHT - 20, PLAYER_WIDTH, PLAYER_HEIGHT)
+    
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break        
+        draw(player)
+
+        keys = pygame.key.get_pressed()
+        
+    
+    pygame.quit()
+
+
+def draw(player):
+    pygame.draw.rect(WINDOW, "red", player)
+    pygame.display.update()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
