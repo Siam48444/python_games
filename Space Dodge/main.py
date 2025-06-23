27be754b852b2e895eccb7aec26ae0bfebf75e88
Siam <siam48444@gmail.com>
2025-06-23 20:38:12 +0600

    siam

--- Space Dodge/main.py
+++ Space Dodge/main.py
@@ -47,7 +47,8 @@ def main():
 def draw(player, elapsed_time):
     WINDOW.fill("black")
 
-    time_text = FONT.render(f"Time: {math.floor(elapsed_time)}s", 1, "white")
+    time_text = FONT.render(f"Time: {math.floor(elapsed_time)}s", 2, "white")
+    WINDOW.blit(time_text, (0, 0))
 
     pygame.draw.rect(WINDOW, "red", player)
     pygame.display.update()
