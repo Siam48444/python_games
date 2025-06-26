a0d4944e24a6511f1a4de0d91a3c04a9c87efd77
Siam <siam48444@gmail.com>
2025-06-26 15:16:17 +0600

    siam

--- Galaxy Fighter/main.py
+++ Galaxy Fighter/main.py
@@ -3,17 +3,17 @@ import os
 
 
 WIDTH, HEIGHT = 1000, 800
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 TITLE = pygame.display.set_caption("Galaxy Fighter")
 
-BG_COLOR = (255, 255, 255)
-FPS = 60
-
 SPACESHIP_YELLOW_IMAGE = pygame.image.load(os.path.join("Assets", "Images", "spaceship_yellow.png"))
 SPACESHIP_RED_IMAGE = pygame.image.load(os.path.join("Assets", "Images", "spaceship_red.png"))
+SPACESHIP_YELLOW = pygame.transform.scale(SPACESHIP_YELLOW_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+SPACESHIP_RED = pygame.transform.scale(SPACESHIP_RED_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
 
-SPACESHIP_YELLOW = pygame.transform.scale(SPACESHIP_YELLOW_IMAGE, (55, 50))
-SPACESHIP_RED = pygame.transform.scale(SPACESHIP_RED_IMAGE, (55, 50))
+BG_COLOR = (255, 255, 255)
+FPS = 60
 
 
 def main():
