e6b19751852c228265797446aee245806ea871a0
Siam <siam48444@gmail.com>
2025-06-26 15:20:06 +0600

    siam

--- Galaxy Fighter/main.py
+++ Galaxy Fighter/main.py
@@ -3,14 +3,15 @@ import os
 
 
 WIDTH, HEIGHT = 1000, 800
-SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 50
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 TITLE = pygame.display.set_caption("Galaxy Fighter")
 
 SPACESHIP_YELLOW_IMAGE = pygame.image.load(os.path.join("Assets", "Images", "spaceship_yellow.png"))
 SPACESHIP_RED_IMAGE = pygame.image.load(os.path.join("Assets", "Images", "spaceship_red.png"))
-SPACESHIP_YELLOW = pygame.transform.scale(SPACESHIP_YELLOW_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
-SPACESHIP_RED = pygame.transform.scale(SPACESHIP_RED_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+
+SPACESHIP_YELLOW = pygame.transform.rotate(pygame.transform.scale(SPACESHIP_YELLOW_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+SPACESHIP_RED = pygame.transform.rotate(pygame.transform.scale(SPACESHIP_RED_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), -90)
 
 BG_COLOR = (255, 255, 255)
 FPS = 60
@@ -32,7 +33,8 @@ def main():
 
 def draw_window():
 	WINDOW.fill(BG_COLOR)
-	WINDOW.blit(SPACESHIP_RED, (0, 0))
+	WINDOW.blit(SPACESHIP_YELLOW, (0, 0))
+	WINDOW.blit(SPACESHIP_RED, (WIDTH - 100, 0))
 	pygame.display.update()	
 
 
