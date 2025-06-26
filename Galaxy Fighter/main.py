c55136d28bff39bb0aec0eb5763001ce538727f4
Siam <siam48444@gmail.com>
2025-06-26 14:19:36 +0600

    siam

--- Galaxy Fighter/main.py
+++ Galaxy Fighter/main.py
@@ -6,11 +6,15 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 TITLE = pygame.display.set_caption("Galaxy Fighter")
 
 BLACK = (0, 0, 0)
+FPS = 60
 
 
 def main():
+	clock = pygame.time.Clock()
+
 	running = True
 	while running:
+		clock.tick(FPS)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
