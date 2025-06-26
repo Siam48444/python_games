2c5ca964a8acb300491f66f1aa3a533ce83eb4cb
Siam <siam48444@gmail.com>
2025-06-26 15:36:36 +0600

    siam

--- Galaxy Fighter/main.py
+++ Galaxy Fighter/main.py
@@ -3,18 +3,17 @@ import os
 
 
 WIDTH, HEIGHT = 1000, 800
-SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 50
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 70, 50
+SPACESHIP_VELOCITY = 5
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 TITLE = pygame.display.set_caption("Galaxy Fighter")
+BG_COLOR = (255, 255, 255)
+FPS = 120
 
 SPACESHIP_YELLOW_IMAGE = pygame.image.load(os.path.join("Assets", "Images", "spaceship_yellow.png"))
 SPACESHIP_RED_IMAGE = pygame.image.load(os.path.join("Assets", "Images", "spaceship_red.png"))
-
 SPACESHIP_YELLOW = pygame.transform.rotate(pygame.transform.scale(SPACESHIP_YELLOW_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), -90)
 SPACESHIP_RED = pygame.transform.rotate(pygame.transform.scale(SPACESHIP_RED_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
-
-BG_COLOR = (255, 255, 255)
-FPS = 60
 
 
 def main():
@@ -28,6 +27,13 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
+
+		keys_pressed = pygame.key.get_pressed()
+		if keys_pressed[pygame.K_w] and red_spaceship.y > 0:
+			red_spaceship.y -= SPACESHIP_VELOCITY
+		if keys_pressed[pygame.K_s] and red_spaceship.y < HEIGHT - SPACESHIP_WIDTH:
+			red_spaceship.y += SPACESHIP_VELOCITY
+
 		draw_window(red_spaceship, yellow_spaceship)
 
 	pygame.quit()
