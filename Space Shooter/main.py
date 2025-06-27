6c311975f10d4f1a1d9245f7668ff01e1a3ece83
Siam <siam48444@gmail.com>
2025-06-27 11:11:27 +0600

    siam

--- Space Shooter/main.py
+++ Space Shooter/main.py
@@ -5,6 +5,7 @@ import time
 pygame.font.init()
 
 
+
 WIDTH, HEIGHT = 800, 800
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 TITLE = pygame.display.set_caption("Space Shooter")
@@ -19,3 +20,35 @@ RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
 GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+BG_COLOR = (0, 0, 0)
+
+
+def main():
+	FPS = 90
+	clock = pygame.time.Clock()
+
+	level = 1
+	lives = 5
+
+	def draw_window():
+		WINDOW.fill(BG_COLOR)
+		pygame.display.update()
+
+	run = True
+	while run:
+		clock.tick(FPS)
+		draw_window()
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+	pygame.quit()	
+
+
+
+
+
+if __name__ == '__main__':
+	main()
