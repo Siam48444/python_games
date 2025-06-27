55dc6e96816cb88e2fc7fcdfea2217764d3bd6af
Siam <siam48444@gmail.com>
2025-06-27 11:50:57 +0600

    siam

--- Space Shooter/main.py
+++ Space Shooter/main.py
@@ -63,6 +63,9 @@ class Enemy(Ship):
 		self.ship_img, self.laser_img = self.COLOR_MAP[color]
 		self.mask = pygame.mask.from_surface(self.ship_img)
 
+	def move(self, vel):
+		self.y += vel
+
 
 
 def main():
