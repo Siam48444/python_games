3249a3fdf215717a5f8eb76155a6392618b63fa5
Siam <siam48444@gmail.com>
2025-06-27 11:02:22 +0600

    siam

--- Space Shooter/main.py
+++ Space Shooter/main.py
@@ -7,6 +7,11 @@ pygame.font.init()
 
 # Load the images
 RED_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
-BLUE_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 GREEN_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 YELLOW_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png")) # Player
+
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
